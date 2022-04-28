03935c547d77cf4c4869edf8cfef7311fda2f7f2
unknown <jimmyale3102@gmail.com>
2022-04-27 19:10:19 -0500

    Add database and dao

---
+++ app/src/main/java/dev/alejo/mylist/feature_note/data/data_source/NoteDatabase.kt
@@ -0,0 +1,13 @@
+package dev.alejo.mylist.feature_note.data.data_source
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import dev.alejo.mylist.feature_note.domain.model.Note
+
+@Database(
+    entities = [Note::class],
+    version = 1
+)
+abstract class NoteDatabase: RoomDatabase() {
+    abstract val noteDao: NoteDao
+}
