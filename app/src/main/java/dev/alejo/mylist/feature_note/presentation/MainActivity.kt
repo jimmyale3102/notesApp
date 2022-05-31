26d6c5a81f3bb141bf31e9a4a67c0465e326152d
unknown <jimmyale3102@gmail.com>
2022-05-31 18:20:07 -0500

    Add navigation controller

--- app/src/main/java/dev/alejo/mylist/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/dev/alejo/mylist/feature_note/presentation/MainActivity.kt
@@ -3,13 +3,16 @@ package dev.alejo.mylist.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dev.alejo.mylist.feature_note.presentation.add_edit_note.components.AddEditNoteScreen
+import dev.alejo.mylist.feature_note.presentation.notes.NotesScreen
+import dev.alejo.mylist.feature_note.presentation.util.Screen
 import dev.alejo.mylist.ui.theme.MyListTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +20,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyListTheme {
-
+                Surface(
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.NotesScreen.route
+                    ) {
+                        composable(route = Screen.NotesScreen.route) {
+                            NotesScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.AddEditNoteScreen.route +
+                                "?noteId={noteId}&noteColor={noteColor}",
+                            arguments = listOf(
+                                navArgument(
+                                    name = "noteId"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                                navArgument(
+                                    name = "noteColor"
+                                ) {
+                                    type = NavType.IntType
+                                    defaultValue = -1
+                                },
+                            )
+                        ) {
+                            val color = it.arguments?.getInt("noteColor") ?: -1
+                            AddEditNoteScreen(
+                                navController = navController,
+                                noteColor = color
+                            )
+                        }
+                    }
+                }
             }
         }
     }
