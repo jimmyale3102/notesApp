983280938bb21c3c4eac8bb3e4d8063d9abaa508
unknown <jimmyale3102@gmail.com>
2022-05-31 18:15:53 -0500

    Add edit note section

---
+++ app/src/main/java/dev/alejo/mylist/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,10 @@
+package dev.alejo.mylist.feature_note.domain.use_case
+
+import dev.alejo.mylist.feature_note.domain.model.Note
+import dev.alejo.mylist.feature_note.domain.repository.NoteRepository
+
+class GetNote(private val repository: NoteRepository) {
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+}
--- app/src/main/java/dev/alejo/mylist/feature_note/domain/use_case/NoteUseCases.kt
+++ app/src/main/java/dev/alejo/mylist/feature_note/domain/use_case/NoteUseCases.kt
@@ -1,6 +1,7 @@
 package dev.alejo.mylist.feature_note.domain.use_case
 
 data class NoteUseCases(
+    val getNote: GetNote,
     val getNotes: GetNotes,
     val deleteNote: DeleteNote,
     val addNote: AddNote
